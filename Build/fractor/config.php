7711cb278d84e39cc93b1f47676eab11ecc6098f
chris <typo3@mediadreams.org>
2026-09-20 08:07:23 +0200

    [TASK] configure Fractor for TYPO3 14

--- Build/fractor/config.php
+++ Build/fractor/config.php
@@ -3,6 +3,8 @@
 declare(strict_types=1);
 
 use a9f\Fractor\Configuration\FractorConfiguration;
+use a9f\Fractor\ValueObject\Indent;
+use a9f\FractorXliff\Configuration\XliffProcessorOption;
 use a9f\Typo3Fractor\Set\Typo3LevelSetList;
 
 $rootPath = dirname(__DIR__, 2);
@@ -16,6 +18,10 @@ return FractorConfiguration::configure()
         $rootPath . '/.Build',
         $rootPath . '/var',
     ])
+    ->withOptions([
+        XliffProcessorOption::INDENT_CHARACTER => Indent::STYLE_TAB,
+        XliffProcessorOption::INDENT_SIZE => 1,
+    ])
     ->withSets([
-        Typo3LevelSetList::UP_TO_TYPO3_13,
+        Typo3LevelSetList::UP_TO_TYPO3_14,
     ]);
